a290e128d35d74e40b117c89bbbac17f15309f5b
Damian Kozlowski <Damian.Kozlowski@bglgroup.co.uk>
2019-02-27 11:40:57 +0000

    add Startup tests

---
+++ Workshop.App.Tests/StartupTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Workshop.App.Tests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _applicationFactory;
+
+        public StartupTests(WebApplicationFactory<Startup> applicationFactory)
+        {
+            _applicationFactory = applicationFactory;
+        }
+
+        [Fact]
+        public async Task MiddlewareTests()
+        {
+            var client = _applicationFactory.CreateClient();
+
+            var response = await client.GetAsync("/swagger/v1/swagger.json");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
